6747ad75c2c11421a641f02a50d959678ef9b65b
[aviv polak] <[avivp0437@gmail.com]>
2021-12-13 13:35:04 +0200

    blog 4.2

---
+++ controllers/blog.js
@@ -0,0 +1,16 @@
+const blogRouter = require("express").Router();
+const Blog = require("../models/blog");
+blogRouter.get("/blogs", (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs);
+    });
+});
+
+blogRouter.post("/blogs", (request, response) => {
+    const blog = new Blog(request.body);
+
+    blog.save().then((result) => {
+        response.status(201).json(result);
+    });
+});
+module.exports = blogRouter;
--- models/note.js
+++ models/note.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  date: Date,
-  important: Boolean,
-})
+    content: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+    date: Date,
+    important: Boolean,
+});
 
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+noteSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema);
