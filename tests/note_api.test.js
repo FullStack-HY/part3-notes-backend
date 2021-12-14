f4a81419a16465b30dc32a25c6aa2240d7b08690
[aviv polak] <[avivp0437@gmail.com]>
2021-12-14 15:56:38 +0200

    4.4

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -18,12 +18,12 @@ const initialNotes = [
 ];
 beforeEach(async () => {
     await Note.deleteMany({});
-    let noteObject = new Note(helper.initialNotes[0]);
-    await noteObject.save();
-    noteObject = new Note(helper.initialNotes[1]);
-    await noteObject.save();
-});
 
+    for (let note of helper.initialNotes) {
+        let noteObject = new Note(note);
+        await noteObject.save();
+    }
+});
 test("notes are returned as json", async () => {
     await api
         .get("/api/notes")
@@ -70,6 +70,35 @@ test("note without content is not added", async () => {
     const notesAtEnd = await helper.notesInDb();
 
     expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
+});
+test("a specific note can be viewed", async () => {
+    const notesAtStart = await helper.notesInDb();
+
+    const noteToView = notesAtStart[0];
+
+    const resultNote = await api
+        .get(`/api/notes/${noteToView.id}`)
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+
+    const processedNoteToView = JSON.parse(JSON.stringify(noteToView));
+
+    expect(resultNote.body).toEqual(processedNoteToView);
+});
+
+test("a note can be deleted", async () => {
+    const notesAtStart = await helper.notesInDb();
+    const noteToDelete = notesAtStart[0];
+
+    await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
+
+    const notesAtEnd = await helper.notesInDb();
+
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
+
+    const contents = notesAtEnd.map((r) => r.content);
+
+    expect(contents).not.toContain(noteToDelete.content);
 });
 afterAll(() => {
     mongoose.connection.close();
